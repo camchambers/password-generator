f1dad33413056dc4fefe6f9a376756bb819edb61
Cameron Chambers <cameronchambers@trentu.ca>
2019-05-23 16:28:58 -0400

    Created project files

---
+++ PasswordGenerator.cs
@@ -0,0 +1,29 @@
+// The System namespace contains fundamental classes and base classes that 
+// define commonly-used value and reference data types, events and event 
+// handlers, interfaces, attributes, and processing exceptions.
+using System;
+
+// The System.Security.Cryptography namespace provides cryptographic services, 
+// including secure encoding and decoding of data, as well as many other 
+// operations, such as hashing, random number generation, and authentication
+using System.Security.Cryptography;
+
+// The System.Text namespace contains classes that represent ASCII and Unicode 
+// character encodings; abstract base classes for converting blocks of 
+// characters to and from blocks of bytes; and a helper class that manipulates 
+// and formats String objects without creating intermediate instances of String.
+using System.Text;
+
+namespace password_generator
+{
+    class PasswordGenerator
+    {
+
+        public PasswordGenerator()
+        {
+            var rng = new RNGCryptoServiceProvider();
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace password_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+        }
+    }
+}
