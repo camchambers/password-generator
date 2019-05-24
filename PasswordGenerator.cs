5197a7213de5cb24e65138154a0f54ffdfef37cc
Cameron Chambers <cameronchambers@trentu.ca>
2019-05-24 10:51:26 -0400

    Completed a working implementation of a password generator

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -18,24 +18,61 @@ namespace password_generator
 {
     class PasswordGenerator
     {
-        private string passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFG" +
-                  "HIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[]{}\\|;:'\",<" +
-                  ".>/?";
 
-        public PasswordGenerator()
+        public string GeneratePassword()
         {
-            Console.WriteLine("Password generator loaded.");
-            var rng = new RNGCryptoServiceProvider();
-        }
+            var randomNumberGenerator = new RNGCryptoServiceProvider();
 
-        public string Generate()
-        {
-            return string.Empty;
-        }
+            // The number of characters in the password that will be generated
+            int passwordSize = 8;
 
-        public char randomCharacter()
-        {
-            return '0';
+            // Lower bound value of unicode table for generating a character
+            int lowerBound = 63;
+
+            // Upper bound value of unicode table for generating a character
+            int upperBound = 122;
+
+            // A random integer derived from our random number byte array
+            uint randomInt = 0;
+
+            // C# string defaults to the UTF-16 encoding of Unicode, 
+            // which is 2 bytes
+            // We create a random number array of 2 bytes in size
+            byte[] randomNumber = new byte[4];
+
+            // A string builder object for storing our password
+            StringBuilder passwordBuffer = new StringBuilder();
+
+            // Generate random characters as many times as the length of 
+            // the password to generate
+            for (int i = 0; i < passwordSize; i++)
+            {
+
+                // Fill the randomNumber byte array with random numbers 
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                // System.BitConverter.ToUInt32 Returns a 32-bit unsigned 
+                // integer converted from four bytes at a specified position 
+                // in a byte array.
+                randomInt = System.BitConverter.ToUInt32(randomNumber, 0);
+
+                // Scale the result between min and max
+                int scaledResult = (int)(randomInt % (upperBound - lowerBound)) + lowerBound;
+
+                // Convert the scaled integer value to a character
+                char resultChar = (char)scaledResult;
+
+                // Add our character to the password buffer
+                passwordBuffer.Append(resultChar);
+
+            }
+
+            // RNGCryptoServiceProvider implements the IDisposable interface
+            // and should be disposed of when finished with it 
+            randomNumberGenerator.Dispose();
+
+            return passwordBuffer.ToString();
+
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace password_generator
     {
         static void Main(string[] args)
         {
+            // Instantiate a new password generator object
             PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+            // Generate a password using the password generator
+            string password = passwordGenerator.GeneratePassword();
+
+            // Display the password
+            Console.WriteLine(password);
         }
     }
 }
